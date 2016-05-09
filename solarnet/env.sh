6cfbd5f11d4180cfd924fe944ae2cbdd8c95bf97
Lars Gierth <larsg@systemli.org>
2016-05-09 20:53:54 +0200

    WIP
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- solarnet/env.sh
+++ solarnet/env.sh
@@ -4,13 +4,42 @@ provsn_hosts=(pluto neptune uranus saturn jupiter venus earth mercury pollux bih
 # provsn_hosts=(pluto neptune uranus saturn jupiter venus earth mercury pollux biham nihal)
 # provsn_hosts=(pluto neptune uranus saturn jupiter venus earth mercury)
 
+for host in ${provsn_hosts[@]}; do
+  domain_record_present "i.ipfs.io" "A" "$host" "$(lookup ip4_address)"
+  domain_record_present "i.ipfs.io" "AAAA" "$host" "$(lookup ip6_address)"
+  domain_record_present "i.ipfs.team" "AAAA" "$host" "$(lookup fc00_address)"
+  domain_record_present "ipfs.io" "A" "@" "$(lookup ip4_address)"
+  domain_record_present "ipfs.io" "AAAA" "@" "$(lookup ip6_address)"
+  domain_record_present "gateway.ipfs.io" "A" "@" "$(lookup ip4_address)"
+  domain_record_present "gateway.ipfs.io" "AAAA" "@" "$(lookup ip6_address)"
+done
+
+# ssh website@pages.ipfs.team
+domain_record_present "ipfs.io" "TXT" "@" "dnslink=/ipns/<hash>"
+# ssh dist@pages.ipfs.team
+domain_record_present "ipfs.io" "CNAME" "dist" "gateway.ipfs.io."
+domain_record_present "ipfs.io" "TXT" "_dnslink.dist" "dnslink=/ipns/<hash>"
+# ssh blog@pages.ipfs.team
+domain_record_present "ipfs.io" "CNAME" "blog" "gateway.ipfs.io."
+domain_record_present "ipfs.io" "TXT" "_dnslink.blog" "dnslink=/ipns/<hash>"
+# ssh refs@pages.ipfs.team
+domain_record_present "ipfs.io" "CNAME" "refs" "gateway.ipfs.io."
+domain_record_present "ipfs.io" "TXT" "_dnslink.refs" "dnslink=/ipns/<hash>"
+# ssh chat@pages.ipfs.team
+domain_record_present "ipfs.io" "CNAME" "chat" "gateway.ipfs.io."
+domain_record_present "ipfs.io" "TXT" "_dnslink.chat" "dnslink=/ipns/<hash>"
+
+for host in ${provsn_hosts[@]}; do
+  domain_record_present "ipfs.fc00.link" "AAAA" "@" "$(lookup fc00_address)"
+done
+domain_record_present "ipfs.fc00.link" "TXT" "_dnslink" "dnslink=/ipns/<hash>"
+
 all_ssh_options="-o ConnectTimeout=5"
 all_omit_build=(secrets/ipfs)
 
-# baseunits=(base docker mtail)
-# gatewayunits=(ipfs ipfs/multireq secrets/ipfs)
-baseunits=(base)
-gatewayunits=()
+baseunits=(base docker)
+gatewayunits=(ipfs ipfs/multireq secrets/ipfs)
+storageunits=(ipfs secrets/ipfs)
 
 pluto_ssh="root@104.236.179.241"
 pluto_units=(${baseunits[@]} ${gatewayunits[@]})
@@ -37,15 +66,15 @@ mercury_ssh="root@178.62.61.185"
 mercury_units=(${baseunits[@]} ${gatewayunits[@]})
 
 pollux_ssh="root@5.9.59.34"
-# pollux_units=(${baseunits[@]} ipfs)
+# pollux_units=(${baseunits[@]} ${storageunits[@]})
 pollux_units=(${baseunits[@]})
 
 biham_ssh="root@188.40.114.11"
-# biham_units=(${baseunits[@]} ipfs)
+# biham_units=(${baseunits[@]} ${storageunits[@]})
 biham_units=(${baseunits[@]})
 
 nihal_ssh="root@188.40.116.66"
-# nihal_units=(${baseunits[@]} ipfs)
+# nihal_units=(${baseunits[@]} ${storageunits[@]})
 nihal_units=(${baseunits[@]})
 
 phobos_ssh="root@104.131.3.162"
